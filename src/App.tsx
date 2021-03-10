74618cce6c1cdb1378fbde1d3b16f78fc82fc577
Alice Huang <alicehccn@gmail.com>
2021-03-10 09:49:57 -0800

    use react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import guitar from './guitar.jpeg';
 import './App.css';
 
-function App() {
-  
-  const onMenuClick = () => {
-    console.log('click')
-  }
-  
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function Product() {
+ return <h2>This is a page for product with ID </h2>;
+}
+
+function AppRouter() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <div className="App-header">
         <h1 className="App-logo">Lodestone</h1>
         <ul className="App-menu-list">
-          <li className="App-menu-item selected" onClick={onMenuClick}>Home</li>
-          <li className="App-menu-item" onClick={onMenuClick}>Music</li>
-          <li className="App-menu-item" onClick={onMenuClick}>Photography</li>
-          <li className="App-menu-item" onClick={onMenuClick}>Events</li>
-          <li className="App-menu-item" onClick={onMenuClick}>About</li>
+          <li className="App-menu-item selected"><Link to="/">Home</Link></li>
+          <li className="App-menu-item"><Link to="/music">Music</Link></li>
+          <li className="App-menu-item"><Link to="/photography">Photography</Link></li>
+          <li className="App-menu-item"><Link to="/events">Events</Link></li>
+          <li className="App-menu-item"><Link to="/about">About</Link></li>
         </ul>
       </div>
       <div>
         <img src={guitar} className="App-background" alt="logo" />
       </div>
     </div>
+    
+        <Route path="/" exact component={Index} />
+        <Route path="/products/:id" component={Product} />
+    </Router>
   );
 }
 
-export default App;
+export default AppRouter;
+  
+
