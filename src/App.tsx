8a0469f3caa16c38731b19c1428eb087354dface
Alice Huang <alicehccn@gmail.com>
2021-03-10 12:17:14 -0800

    ugly solution to header class problem

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,21 @@ interface PropTypes {
 }
 
 function AppRouter(props: PropTypes) {
-  const [page, setPage] = useState('/')
+const [page, setPage] = useState('/')
   useEffect(() => {
     console.log(page)
   }, [page]);
 
   return (
-      <div className="App">
+      <div className={`App page-${page}`}>
         <div className="App-header">
           <h1 className="App-logo">Lodestone</h1>
           <ul className="App-menu-list">
-            <li className="App-menu-item"><Link onClick={() => setPage('home')} to="/">Home</Link></li>
-            <li className="App-menu-item"><Link onClick={() => setPage('music')} to="/music">Music</Link></li>
-            <li className="App-menu-item"><Link onClick={() => setPage('photography')} to="/photography">Photography</Link></li>
-            <li className="App-menu-item"><Link onClick={() => setPage('events')} to="/events">Events</Link></li>
-            <li className="App-menu-item"><Link onClick={() => setPage('about')} to="/about">About</Link></li>
+            <li className="App-menu-item home"><Link onClick={() => setPage('home')} to="/">Home</Link></li>
+            <li className="App-menu-item music"><Link onClick={() => setPage('music')} to="/music">Music</Link></li>
+            <li className="App-menu-item photography"><Link onClick={() => setPage('photography')} to="/photography">Photography</Link></li>
+            <li className="App-menu-item events"><Link onClick={() => setPage('events')} to="/events">Events</Link></li>
+            <li className="App-menu-item about"><Link onClick={() => setPage('about')} to="/about">About</Link></li>
           </ul>
         </div>
       {props.children}
