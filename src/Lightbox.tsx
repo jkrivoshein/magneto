79c826a2aa4101be823d82821443115af55fa826
Alice Huang <alicehccn@gmail.com>
2021-03-11 23:20:11 -0800

    add files

---
+++ src/Lightbox.tsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Photo } from './actions/types'
+import './Lightbox.scss'
+
+interface PropTypes {
+  photo: Photo,
+  history: any,
+}
+
+class Lightbox extends Component<PropTypes, PropTypes> {
+
+  onCloseClick() {
+    this.props.history.goBack()
+  }
+
+  render () {
+    const { photo } = this.props
+    if (!photo) return null
+
+    return (
+      <div>
+        <div className="photo-wrapper" onClick={this.onCloseClick.bind(this)}>
+          <div className='photo-main'>
+            <div className='photo-content'>
+              <img alt={photo.title} src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_z.jpg`} />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps (state: any) {
+  return {
+    photo: state.photos.photo,
+  }
+}
+
+export default connect(mapStateToProps, {})(Lightbox)
