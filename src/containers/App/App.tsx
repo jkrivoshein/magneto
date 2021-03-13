1751895b372797ece5fdf6c4a2e53214cb82b2bd
Alice Huang <alicehccn@gmail.com>
2021-03-12 20:07:27 -0800

    burger menu

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { Link } from "react-router-dom";
+import { slide as Menu } from 'react-burger-menu'
 
 import './App.scss';
 
@@ -9,7 +10,13 @@ interface PropTypes {
 
 function AppRouter(props: PropTypes) {
   const [page, setPage] = useState('home')
-
+  const [isOpen, setState] = useState(false)
+  const toggleMenu = (page?: string) => {
+    setState(!isOpen)
+    if (page)
+      setPage(page)
+  }
+  console.log(isOpen)
   return (
     <div className={`App page-${page}`}>
       <div className="App-header">
@@ -21,6 +28,15 @@ function AppRouter(props: PropTypes) {
           <li className="App-menu-item events"><Link onClick={() => setPage('events')} to="/events">Events</Link></li>
           <li className="App-menu-item about"><Link onClick={() => setPage('about')} to="/about">About</Link></li>
         </ul>
+        <div onClick={() => toggleMenu()}>
+          <Menu right isOpen={isOpen}>
+            <Link className="menu-item" onClick={() => toggleMenu('home')} to="/">Home</Link>
+            <Link className="menu-item" onClick={() => toggleMenu('music')} to="/music">Music</Link>
+            <Link className="menu-item" onClick={() => toggleMenu('photography')} to="/photography">Photography</Link>
+            <Link className="menu-item" onClick={() => toggleMenu('events')} to="/events">Events</Link>
+            <Link className="menu-item" onClick={() => toggleMenu('about')} to="/about">About</Link>
+          </Menu>
+        </div>
       </div>
       {props.children}
       <footer>
