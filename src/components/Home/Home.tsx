4f0edf9811019048c8360c5de9bc6ba3c3a5e0fb
Alice Huang <alicehccn@gmail.com>
2021-03-12 16:45:34 -0800

    stage name

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,7 +3,7 @@ import './Home.scss';
 const Home = () => {
   return (
     <div className="home-container">
-      <h1>Josh Krivoshein</h1>
+      <h1>Josh K.'s Lodestone</h1>
       <p>Jus’ chillin.</p>
     </div>
   );
