24c50235b1cc466dd17fb28182b06bb2342b18f7
Alice Huang <alicehccn@gmail.com>
2021-03-12 21:43:05 -0800

    lightbox max width/height

--- src/components/Lightbox/Lightbox.tsx
+++ src/components/Lightbox/Lightbox.tsx
@@ -23,7 +23,7 @@ class Lightbox extends Component<PropTypes, PropTypes> {
         <div className="photo-wrapper" onClick={this.onCloseClick.bind(this)}>
           <div className='photo-main'>
             <div className='photo-content'>
-              <img alt={photo.title} src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_c.jpg`} />
+              <img alt={photo.title} max-width="90vw" src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_c.jpg`} />
             </div>
           </div>
         </div>
