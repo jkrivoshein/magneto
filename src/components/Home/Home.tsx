0d91efbb6a7c87eb46111bdfc103c148679b89a7
Alice Huang <alicehccn@gmail.com>
2021-03-12 21:58:40 -0800

    social link fb

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,6 +5,9 @@ const Home = () => {
     <div className="home-container">
       <h1>Josh K's lodestone</h1>
       <p>Jus’ chillin.</p>
+      <div className="social-links">
+        <a target="_blank" title="follow me on facebook" href="https://www.facebook.com/PLACEHOLDER"><img alt="follow me on facebook" src="/fb-icon.png" /></a>
+      </div>
     </div>
   );
 }
