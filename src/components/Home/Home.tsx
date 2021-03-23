7bf04570244dd6cc36dc35d8ff51f03e488a8130
Alice Huang <alicehccn@gmail.com>
2021-03-23 12:37:12 -0700

    noreferrer for external links

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,10 +6,10 @@ const Home = () => {
       <h1>Josh K's lodestone</h1>
       <p>Jus’ chillin.</p>
       <div className="social-links">
-        <a target="_blank" title="follow me on facebook" href="https://www.facebook.com/djLodestone"><img alt="follow me on facebook" src="/fb-icon.png" /></a>
-        <a target="_blank" title="follow me on SoundCloud" href="https://soundcloud.com/djlodestone"><img alt="follow me on SoundCloud" src="/soundcloud-icon.png"/></a>
-        <a target="_blank" title="follow me on flickr" href="https://www.flickr.com/photos/jzf_k"><img alt="follow me on flickr" src="/flickr-icon.png"/></a>
-        <a title="email me" href="mailto:lodestonetx@yahoo.com"><img alt="email me" src="/email-icon.png"/></a>
+        <a rel="noreferrer" target="_blank" title="follow me on facebook" href="https://www.facebook.com/djLodestone"><img alt="follow me on facebook" src="/fb-icon.png" /></a>
+        <a rel="noreferrer" target="_blank" title="follow me on SoundCloud" href="https://soundcloud.com/djlodestone"><img alt="follow me on SoundCloud" src="/soundcloud-icon.png"/></a>
+        <a rel="noreferrer" target="_blank" title="follow me on flickr" href="https://www.flickr.com/photos/jzf_k"><img alt="follow me on flickr" src="/flickr-icon.png"/></a>
+        <a rel="noreferrer" title="email me" href="mailto:lodestonetx@yahoo.com"><img alt="email me" src="/email-icon.png"/></a>
       </div>
     </div>
   );
