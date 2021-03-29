816c4ba90fda80ff584ce7696d5079f03f02a760
Alice Huang <alicehccn@gmail.com>
2021-03-28 17:44:27 -0700

    use gist for about

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -3,8 +3,6 @@ import './About.scss'
 const About = () => {
   return (
     <div className="about-container">
-      <h2>A: "say something."</h2>
-      <h2>J: "something."</h2>
     </div>
   );
 }
