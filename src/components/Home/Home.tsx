514f720a618a9a802f8c68ac5d41e669ea71fd99
jkrivoshein <jkrivoshein@gmail.com>
2025-05-29 19:05:50 -0700

    Jkrivoshein patch 1 (#2)
    * Update Home.tsx
    
    * Update Home.scss

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,15 +3,46 @@ import './Home.scss';
 const Home = () => {
   return (
     <div className="home-container">
-      <h1>Josh K's lodestone</h1>
-      <p>Jus’ chillin.</p>
-      <div className="social-links">
-        <a rel="noreferrer" target="_blank" title="follow me on facebook" href="https://www.facebook.com/djLodestone"><img alt="follow me on facebook" src="/fb-icon.png" /></a>
-        <a rel="noreferrer" target="_blank" title="follow me on SoundCloud" href="https://soundcloud.com/djlodestone"><img alt="follow me on SoundCloud" src="/soundcloud-icon.png"/></a>
-        <a rel="noreferrer" target="_blank" title="follow me on flickr" href="https://www.flickr.com/photos/jzf_k"><img alt="follow me on flickr" src="/flickr-icon.png"/></a>
-        <a rel="noreferrer" title="email me" href="mailto:lodestonetx@yahoo.com"><img alt="email me" src="/email-icon.png"/></a>
-      </div>
+      <header className="hero">
+        <h1>Lodestone</h1>
+        <p className="tagline">The Lodestone project is an ongoing exploration of dimensional perception.</p>
+      </header>
+
+      <section className="social-links">
+        <a
+          rel="noreferrer"
+          target="_blank"
+          title="Follow me on Facebook"
+          href="https://www.facebook.com/djLodestone"
+        >
+          <img alt="Facebook" src="/fb-icon.png" />
+        </a>
+        <a
+          rel="noreferrer"
+          target="_blank"
+          title="Follow me on SoundCloud"
+          href="https://soundcloud.com/djlodestone"
+        >
+          <img alt="SoundCloud" src="/soundcloud-icon.png" />
+        </a>
+        <a
+          rel="noreferrer"
+          target="_blank"
+          title="Follow me on Flickr"
+          href="https://www.flickr.com/photos/jzf_k"
+        >
+          <img alt="Flickr" src="/flickr-icon.png" />
+        </a>
+        <a
+          rel="noreferrer"
+          title="Email me"
+          href="mailto:lodestonetx@yahoo.com"
+        >
+          <img alt="Email" src="/email-icon.png" />
+        </a>
+      </section>
     </div>
   );
-}
+};
+
 export default Home;
